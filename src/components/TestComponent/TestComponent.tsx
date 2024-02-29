8a1769016923a91224c250aeca2957737643e8b7
Thomas Mills <mills.thoma@gmail.com>
2024-02-29 14:31:50 +0100

    TestComponent setup with markdown examples of code snippet and embedded react component

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,10 +1,12 @@
 import PlaceholderImage from '@site/static/img/undraw_docusaurus_mountain.svg'
 
-function TestComponent(): JSX.Element {
+function TestComponent({ children = undefined, foo = 'bar' }): JSX.Element {
   return (
-    <div>
-      <PlaceholderImage />
+    <div style={{ border: '2px solid #ddd', padding: '10px' }}>
+      <PlaceholderImage style={{ maxWidth: '200px', height: '200px' }} />
+      <h1>{foo}</h1>
       <p>Lorem ipsum blah blah blah</p>
+      {children && <div>{children}</div>}
     </div>
   );
 }
