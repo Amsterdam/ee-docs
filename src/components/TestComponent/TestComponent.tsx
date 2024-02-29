270acf3dbc9336665ba45fdbdc426d0552114cce
Thomas Mills <mills.thoma@gmail.com>
2024-02-29 13:18:14 +0100

    Initial docusaurus test setup

---
+++ src/components/TestComponent/TestComponent.tsx
@@ -0,0 +1,12 @@
+import PlaceholderImage from '@site/static/img/undraw_docusaurus_mountain.svg'
+
+function TestComponent(): JSX.Element {
+  return (
+    <div>
+      <PlaceholderImage />
+      <p>Lorem ipsum blah blah blah</p>
+    </div>
+  );
+}
+
+export default TestComponent;
