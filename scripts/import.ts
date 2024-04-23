b2d74255ff63d15e3220c3d3c34745149a6a60a6
Thomas Mills <mills.thoma@gmail.com>
2024-04-23 11:36:52 +0200

    Command added to import docs from development-standards repo

---
+++ scripts/import.ts
@@ -0,0 +1,48 @@
+import { simpleGit, SimpleGit, CleanOptions } from 'simple-git';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const git: SimpleGit = simpleGit().clean(CleanOptions.FORCE);
+const remoteUrl = 'https://github.com/Amsterdam/development-standards';
+const localDir = 'docs';
+const cloneDir = path.join(localDir, 'latest');
+
+// Cleanup previously cloned files from development-standards repo
+const cleanupOldFiles = () => {
+  const excludeFiles = ['intro.md'];
+
+  fs.readdirSync(localDir).forEach((file) => {
+    if (!excludeFiles.includes(file)) {
+      const filePath = path.join(localDir, file);
+      fs.rmSync(filePath, { recursive: true });
+    }
+  });
+};
+
+// Sort and cleanup the cloned repository files
+const cleanupRepoFiles = () => {
+  const dirs = ['backend', 'cloud', 'frontend', 'standards'];
+
+  // For each directory remove the README and move the directory up a level
+  dirs.forEach((dir) => {
+    const readmePath = path.join(cloneDir, `${dir}/README.md`);
+
+    if (fs.existsSync(readmePath)) {
+      fs.rmSync(path.join(cloneDir, `${dir}/README.md`));
+    }
+
+    fs.renameSync(path.join(cloneDir, dir), path.join(localDir, dir))
+  })
+
+  // Erase repo directory
+  fs.rmSync(cloneDir, { recursive: true });
+};
+
+if (fs.existsSync(localDir)) {
+  cleanupOldFiles();
+}
+
+// Clone the latest development-standards repo
+git.clone(remoteUrl, cloneDir)
+  .then(() => cleanupRepoFiles())
+  .catch((err) => console.error('failed: ', err));
