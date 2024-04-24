5ce27ba8467f96ee21ff527d984a6dee2cf885ee
Thomas Mills <mills.thoma@gmail.com>
2024-04-24 15:14:46 +0200

    Styles clean up to align with amsterdam-design-system container

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -48,7 +48,11 @@ const config: Config = {
         //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         // },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: [
+            './src/css/custom.css',
+            './src/css/ams-overrides.css',
+            './src/css/doc-overrides.css',
+          ],
         },
       } satisfies Preset.Options,
     ],
