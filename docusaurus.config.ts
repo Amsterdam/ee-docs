6455158d828c9ddbea881740d66fd42cfdab7943
Thomas Mills <mills.thoma@gmail.com>
2024-08-01 15:20:14 +0200

    Update docusaurus config url (#35)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://amsterdam.github.io',
+  url: 'https://developers.amsterdam',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
