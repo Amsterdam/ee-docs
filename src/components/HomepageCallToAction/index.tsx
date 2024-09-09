fae91fa8fe07336a2c41fa95c2e2d355dd4f4541
SireeKoolenWijkstra <sireekoolenwijkstra@gmail.com>
2024-09-09 09:30:21 +0200

    Make the new intro blocks, text shown is dependent on whether a rolling ball is visible.
    * Insert Grid with two Grid.Cells span=7 and span=5 so that total is still 12, but picture will be on the right side of the screen
    
    * Insert Grid with Grid.Cell span=4 for Guidelines block so that no more then 3 cards next to each other will be shown
    -- add all icons that Linda (the designer) send
    
    * Replace Icon, Link, Header and Paragraph with Card for better alignment with design system
    -- aspect ratio surrounding image/icons has been deleted since the images are more alike icons then images and they have a fixed width and height
    -- used GithubIcon for image/ icon for card about using Git since that seems most applicable image
    -- introduced check for type of icon so that GitHubIcon can be used as well
    
    * Introduce ShareButton
    -- color dependent on darkmode
    
    * Show not Image of Amsterdam on mobile
    -- clean up styling, relegate to .css files
    -- decide to not implement background color and different font sizing since this story is getting quite bloated. New story COM-265 for background color and COM-266 for font-sizing.

--- src/components/HomepageCallToAction/index.tsx
+++ src/components/HomepageCallToAction/index.tsx
@@ -1,22 +1,45 @@
 import React from 'react';
-import Link from '@docusaurus/Link';
-import { Grid, Heading, Paragraph } from '@amsterdam/design-system-react';
+import { Grid, Heading, Image, Overlap, Paragraph } from '@amsterdam/design-system-react';
+import ShareButton from '@site/src/theme/Button';
 
-const HomepageCallToAction = () => (
-  <Grid.Cell span={12}>
-    <Heading size="level-4">Greetings!</Heading>
-    <Paragraph>
-      Welcome to our website. Here, you&apos;ll discover an overview of all the standards and shared
-      components maintained by the municipality of Amsterdam. While the website is still under
-      development, we cordially invite contributions from our developers. Let&apos;s collaborate to
-      enhance this platform together!{' '}
-    </Paragraph>
-    <Paragraph>
-      {' '}
-      Discover how you can contribute on{' '}
-      <Link to="/docs/intro">our page called &quot;Collaborating on standards&quot;.</Link>
-    </Paragraph>
-  </Grid.Cell>
-);
+interface HomepageCallToActionProps {
+  isBallRolling?: boolean;
+  isImageShowing?: boolean;
+}
+const HomepageCallToAction = ({
+  isBallRolling = true,
+  isImageShowing = true,
+}: HomepageCallToActionProps) => {
+  return (
+    <Grid.Cell span={12}>
+      <Overlap>
+        <Grid className="ams-grid_inner">
+          <Grid.Cell span={7}>
+            <Heading size="level-2">
+              {isBallRolling ? "Let's get the ball rollin'" : 'Contribute'}
+            </Heading>
+            <Paragraph>
+              Developers.amsterdam offers an overview of the standards and shared components
+              maintained by the municipality of Amsterdam. Contributions to enhance the platform are
+              encouraged, with resources available to learn how to get involved.
+            </Paragraph>
+            <br />
+            <ShareButton />
+          </Grid.Cell>
+          {isImageShowing && (
+            <Grid.Cell span={5}>
+              <Image
+                className="ams-image_responsive"
+                alt=""
+                loading="lazy"
+                src="img/aanzicht_Amsterdam.png"
+              />
+            </Grid.Cell>
+          )}
+        </Grid>
+      </Overlap>
+    </Grid.Cell>
+  );
+};
 
 export default HomepageCallToAction;
