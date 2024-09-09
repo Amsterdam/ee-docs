230983e75a608b8c3e04085d26ca140f0d13edf9
SireeKoolenWijkstra <sireekoolenwijkstra@gmail.com>
2024-09-09 09:30:21 +0200

    Make the new intro blocks, text shown is dependent on whether a rolling ball is visible.
    * Insert Grid with two Grid.Cells span=7 and span=5 so that total is still 12, but picture will be on the right side of the screen
    
    * Insert Grid with Grid.Cell span=4 for Guidelines block so that no more then 3 cards next to each other will be shown
    -- add all icons that Linda (the designer) send
    
    * Replace Icon, Link, Header and Paragraph with Card for better alignment with design system
    -- aspect ratio surrounding image/icons has been deleted since the images are more alike icons then images and they have a fixed width and height
    -- used GithubIcon for image/ icon for card about using Git since that seems most applicable image
    -- introduced check for type of icon so that GitHubIcon can be used as well
    
    * Introduce ShareButton
    -- color dependent on darkmode
    
    * Show not Image of Amsterdam on mobile
    -- clean up styling, relegate to .css files
    -- decide to not implement background color and different font sizing since this story is getting quite bloated. New story COM-265 for background color and COM-266 for font-sizing.

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import Link from '@docusaurus/Link';
-import { Grid, Heading, Paragraph, UnorderedList } from '@amsterdam/design-system-react';
-
+import { Card, Grid, Heading, Paragraph, Image } from '@amsterdam/design-system-react';
+import GitHubIcon from '@site/static/img/github-mark.svg';
 interface FeatureItem {
   title: string;
   to: string;
   description: string;
+  image: string | React.ComponentType<React.SVGProps<SVGSVGElement>>;
 }
 
 const featureList: FeatureItem[] = [
   {
+    image: 'img/W3C.svg',
     title: 'Accessibility',
     to: '/docs/frontend/accessibility',
     description:
       'In accordance with the Digital Government Act, the municipality of Amsterdam is required to build all its websites and applications in compliance with the Web Content Accessibility Guidelines (WCAG) at levels A and AA.',
   },
   {
+    image: 'img/Documentation.svg',
     title: 'Documentation',
     to: '/docs/general/project-documentation',
     description:
@@ -28,6 +30,7 @@ const featureList: FeatureItem[] = [
   //   description: 'Code reviews are required before merging to main',
   // },
   {
+    image: 'img/Git.svg',
     title: 'Storing source code',
     to: '/docs/general/storing-source-code',
     description: 'We use Git to store our source code. ',
@@ -43,29 +46,34 @@ const featureList: FeatureItem[] = [
   //   description: 'A guideline on how to choose',
   // },
   {
+    image: GitHubIcon,
     title: 'Using Git',
     to: '/docs/general/using-git',
     description: 'Our policy how to use Git',
   },
   {
+    image: 'img/react.svg',
     title: 'Frontend - Languages and Frameworks (Not yet formatted to meet the required standard.)',
     to: '/docs/frontend/languages-and-frameworks',
     description:
       'For all frontend projects within de Municipality of Amsterdam we choose to work with React and its ecosystem.',
   },
   {
+    image: 'img/shared.svg',
     title: 'Frontend - Shared components (Not yet formatted to meet the required standard.)',
     to: '/docs/frontend/shared-components',
     description:
       'A list of components that our being used by the developers of the city of Amsterdam',
   },
   {
+    image: 'img/third_party.svg',
     title: 'Frontend - Third party dependencies (Not yet formatted to meet the required standard.)',
     to: '/docs/frontend/third-party-dependencies',
     description:
       ' A list of recommended packages and set guidelines for choosing a package which is not on the list.',
   },
   {
+    image: 'img/testing.svg',
     title: 'Frontend - Testing (Not yet formatted to meet the required standard.)',
     to: '/docs/frontend/testing',
     description: 'A guideline to testing for frontenders.',
@@ -74,25 +82,27 @@ const featureList: FeatureItem[] = [
 
 export default function HomepageFeatures(): JSX.Element {
   const list = featureList.map((listItem) => (
-    <div key={uuidv4()}>
-      <UnorderedList>
-        <UnorderedList.Item>
-          <Heading size="level-5">
-            <Link to={listItem.to}>{listItem.title}</Link>
-          </Heading>
-          <Paragraph>{listItem.description}</Paragraph>
-        </UnorderedList.Item>
-      </UnorderedList>
-    </div>
+    <Grid.Cell key={uuidv4()} span={4}>
+      <Card>
+        {typeof listItem.image === 'string' ? (
+          <Image alt="" src={listItem.image} loading="lazy" />
+        ) : (
+          <listItem.image width="62" height="62" preserveAspectRatio="xMinYMin" />
+        )}
+        <Heading size="level-4">
+          <Card.Link href={listItem.to} />
+        </Heading>
+        <Paragraph size="small">{listItem.description}</Paragraph>
+      </Card>
+    </Grid.Cell>
   ));
 
   return (
     <>
       <Grid.Cell span={12}>
-        <Heading size="level-4">Current Documented Standards</Heading>
-      </Grid.Cell>
-      <Grid.Cell span={12}>
-        <div>{list}</div>
+        <Heading size="level-3">Guidelines</Heading>
+
+        <Grid className="ams-grid_inner">{list}</Grid>
       </Grid.Cell>
     </>
   );
