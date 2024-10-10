6e5382684c2784dd5fe59c7ee1a769fee6508bf9
SireeKoolenWijkstra <sireekoolenwijkstra@gmail.com>
2024-10-10 09:48:02 +0200

    WIP resolve feedback

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -90,6 +90,7 @@ export default function HomepageFeatures(): JSX.Element {
           <listItem.image width="62" height="62" preserveAspectRatio="xMinYMin" />
         )}
         <Heading size="level-4">
+          {listItem.title}
           <Card.Link href={listItem.to} />
         </Heading>
         <Paragraph size="small">{listItem.description}</Paragraph>
@@ -100,7 +101,9 @@ export default function HomepageFeatures(): JSX.Element {
   return (
     <>
       <Grid.Cell span={12}>
-        <Heading size="level-3">Guidelines</Heading>
+        <Heading className="ams-mb--sm" size="level-3">
+          Guidelines
+        </Heading>
 
         <Grid className="ams-grid_inner">{list}</Grid>
       </Grid.Cell>
