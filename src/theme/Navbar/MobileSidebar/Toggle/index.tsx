3158e5fba5a26d80c99f2e8ccd17f6f4afaff383
Thomas Mills <mills.thoma@gmail.com>
2024-11-06 18:03:24 +0100

    mobile search button added

--- src/theme/Navbar/MobileSidebar/Toggle/index.tsx
+++ src/theme/Navbar/MobileSidebar/Toggle/index.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import { useNavbarMobileSidebar } from '@docusaurus/theme-common/internal';
 import { translate } from '@docusaurus/Translate';
+import { Icon } from '@amsterdam/design-system-react';
+import { MenuIcon, SearchIcon } from '@amsterdam/design-system-react-icons';
+import styles from './styles.module.css';
 
 export default function MobileSidebarToggle(): JSX.Element {
   const { toggle, shown } = useNavbarMobileSidebar();
   return (
-    <button
-      onClick={toggle}
-      aria-label={translate({
-        id: 'theme.docs.sidebar.toggleSidebarButtonAriaLabel',
-        message: 'Toggle navigation bar',
-        description: 'The ARIA label for hamburger menu button of mobile navigation',
-      })}
-      aria-expanded={shown}
-      className="ams-header__menu-button clean-btn"
-      type="button"
-    >
-      Menu
-    </button>
+    <div className={styles.container}>
+      <button type="button" className={`${styles.button} clean-btn`}>
+        <Icon svg={SearchIcon} />
+      </button>
+      <button
+        onClick={toggle}
+        aria-label={translate({
+          id: 'theme.docs.sidebar.toggleSidebarButtonAriaLabel',
+          message: 'Toggle navigation bar',
+          description: 'The ARIA label for hamburger menu button of mobile navigation',
+        })}
+        aria-expanded={shown}
+        className={`${styles.button} clean-btn`}
+        type="button"
+      >
+        <Icon svg={MenuIcon} />
+      </button>
+    </div>
   );
 }
