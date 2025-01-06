3158206b9d95a11f2cb52a73c912ea55e6ef284b
Siree Koolen-Wijkstra <sireekoolenwijkstra@gmail.com>
2025-01-06 14:55:11 +0100

    ci(build and deploy): com-178 release to main triggers automatic deploy  (#94)
    Added an deploy yml with the config of docusaurus (https://docusaurus.io/docs/deployment#triggering-deployment-with-github-actions)

---
+++ build/assets/js/c141421f.cec688b1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocusaurus=self.webpackChunkdocusaurus||[]).push([["738"],{3491:function(u){u.exports=JSON.parse('{"name":"docusaurus-theme-search-algolia","id":"default"}')}}]);
