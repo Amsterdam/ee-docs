a4499db83c5e30d338fe15708fb829bec30ef7a6
Thomas Mills <mills.thoma@gmail.com>
2026-02-23 17:00:47 +0100

    dark and light variants of logos added

--- docs/static/stack/_components/LogoGrid.tsx
+++ docs/static/stack/_components/LogoGrid.tsx
@@ -1,28 +1,62 @@
 import React, { CSSProperties, ReactNode } from 'react';
+import { useColorMode } from '@docusaurus/theme-common';
 import styles from './LogoGrid.module.css';
 
 interface LogoGridProps {
-  images: ReactNode[];
+  images: (ReactNode | { light: ReactNode; dark: ReactNode })[];
   columns?: number;
   logoHeight?: number;
 }
 
-const LogoGrid = ({ images, columns = 5, logoHeight = 40 }: LogoGridProps) => (
-  <div
-    className={styles.root}
-    style={
-      {
-        '--columns': columns,
-        '--logo-height': `${logoHeight}px`,
-      } as CSSProperties
+const LogoGrid = ({ images, columns = 5, logoHeight = 40 }: LogoGridProps) => {
+  const { colorMode } = useColorMode();
+  const logoElements = images.map((image, index) => {
+    if (!image) return;
+
+    if (
+      Object.prototype.hasOwnProperty.call(image, 'light') &&
+      Object.prototype.hasOwnProperty.call(image, 'dark')
+    ) {
+      if (colorMode === 'dark') {
+        return (
+          <div key={index} className={styles.item}>
+            {image.dark}
+          </div>
+        );
+      }
+
+      return (
+        <div key={index} className={styles.item}>
+          {image.light}
+        </div>
+      );
     }
-  >
-    {images.map((image, index) => (
+
+    return (
       <div key={index} className={styles.item}>
         {image}
       </div>
-    ))}
-  </div>
-);
+    );
+  });
+
+  return (
+    <div
+      className={styles.root}
+      style={
+        {
+          '--columns': columns,
+          '--logo-height': `${logoHeight}px`,
+        } as CSSProperties
+      }
+    >
+      {logoElements}
+      {/* {images.map((image, index) => (
+        <div key={index} className={styles.item}>
+          {image}
+        </div>
+      ))} */}
+    </div>
+  );
+};
 
 export default LogoGrid;
