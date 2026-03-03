970242f54ea99be7acea2d39ed035afe0a4602b4
Abdellah <abdellah11@live.nl>
2026-03-03 16:55:25 +0100

    Adding github copilot documentation

--- sidebars.ts
+++ sidebars.ts
@@ -27,6 +27,7 @@ const sidebars: SidebarsConfig = {
         'general/testing',
         'general/third-party-dependencies',
         'general/using-git',
+        'general/github-copilot',
       ],
     },
     {
